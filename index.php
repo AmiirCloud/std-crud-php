a52f62ca8ffc42a33dff0fd83967ab2dbb5bebc5
AmirMuse <amirlearn08@gmail.com>
2023-08-09 17:41:58 -0700

    Boot strap girid Layouts

---
+++ index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+
+  <body>
+
+<div class="container">
+    <div class="row">
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3 border hover:text-info  shadow-lg p-3 px-4 text-left mx-2 mw-100">
+            <h1 class="fw-light">Post</h1>
+                <p class="text-left lead fw-light lh-base">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi reprehenderit ad natus hic doloribus quos quasi pariatur nulla nam magnam, harum quidem exercitationem, accusamus ipsam unde. Natus et minima inventore!
+                </p>
+        </div>    <div class="col-12 col-sm-6 col-md-4 col-lg-3 border hover:text-info  shadow-lg p-3 px-4 text-left mx-2">
+            <h1 class="fw-light">Post</h1>
+                <p class="text-left lead fw-light lh-base">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi reprehenderit ad natus hic doloribus quos quasi pariatur nulla nam magnam, harum quidem exercitationem, accusamus ipsam unde. Natus et minima inventore!
+                </p>
+        </div>    <div class="col-12 col-sm-6 col-md-4 col-lg-3 border hover:text-info  shadow-lg p-3 px-4 text-left mx-2">
+            <h1 class="fw-light">Post</h1>
+                <p class="text-left lead fw-light lh-base">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi reprehenderit ad natus hic doloribus quos quasi pariatur nulla nam magnam, harum quidem exercitationem, accusamus ipsam unde. Natus et minima inventore!
+                </p>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 border hover:text-info  shadow-lg p-3 px-4 text-left mx-2">
+            <h1 class="fw-light">Post</h1>
+                <p class="text-left lead fw-light lh-base">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi reprehenderit ad natus hic doloribus quos quasi pariatur nulla nam magnam, harum quidem exercitationem, accusamus ipsam unde. Natus et minima inventore!
+                </p>
+        </div>
+       
+    </div>
+</div>
+    
+   <!-- <h1 class="text-center fs-1 bg-danger ">Hello, world!</h1>
+    <h1 class="text-center fs-2 bg-yellow">Hello, world!</h1>
+    <h1 class="text-center fs-3 bg-success">Hello, world!</h1>
+    <h1 class="text-center fs-4 bg-dark text-white">Hello, world!</h1>
+    <h1 class="text-center fs-5 bg-info">Hello, world!</h1>
+    <h1 class="text-center">Hello, world!</h1> -->
+   </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
