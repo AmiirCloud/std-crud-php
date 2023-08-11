68d1be3d91fffb6af0e65b007518cd185beff73a
AmirMuse <amirlearn08@gmail.com>
2023-08-10 23:43:53 -0700

    Update Api added

--- crud-with-ajax/api.php
+++ crud-with-ajax/api.php
@@ -4,11 +4,16 @@ header('Content-type:application/json');
 // read 
 // update 
 // delete 
-// $action = '';
+$action = '';
 if(isset($_POST['action'])){
 
     $action = $_POST['action'];
 }
+if(isset($GET['id'])){
+    $action =  'update';
+    // $action = $;_POST['action'];
+}
+
 // write readAll Student api 
 function readAllStd($conn){
   $data  = array();
@@ -47,10 +52,40 @@ function register($con){
     echo json_encode($message);
 }
 
-if(isset($action)){
+function update($con){
+
+    $message  = array();
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        $name = $_GET['name'];
+        $class = $_GET['class'];
+        $phone = $_GET['phone'];
+        $address = $_GET['address'];
+
+        $query = "UPDATE sudents set name = '$name',class = '$class' , phone = '$phone' , address = '$address' where id   = '$id'";
+        $res = $con->quer($query);
+        if($res){
+            $message =  array('status',true,'data'=>"Updated Success");
+
+        }else{
+            $message  =  array('status',false,'data'=>$con->error);
+        }
+    }else{
+        $message = array('status'=>false,'data'=>'GEt Reuired');
+        // echo json_encode($message)
+    }
+    echo json_encode($message);
+}
+
+if(isset($_POST['action'])){
     $action($con);
 }else{
-    echo 'action is required';
+    echo 'there is no action';
+}
+if(isset($_GET['id'])){
+    // echo ('we dont have any get');
+}else{
+    echo 'we dont have any get is required';
 }
 
 ?>
